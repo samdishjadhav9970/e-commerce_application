e1460316a74cf0099c545b981d8c28b2a0ec4cf3
samdish jadhav <samdishjadhav9970@gmail.com>
2023-12-30 22:43:09 +0530

    NewCollections and NewsLetter component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+import FooterLogo from '../Assets/logo_big.png'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className="footer-logo">
+            <img src={FooterLogo} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Footer
